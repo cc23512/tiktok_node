17cfb372c4cb99edeecdd0ecc5c920c77ce57735
cc23512 <cc23512@g.unicamp.br>
2023-11-30 22:57:52 -0300

    controller e dao

---
+++ src/app/bd/dao_ttk.js
@@ -0,0 +1,8 @@
+class tikTokDAO{
+
+    constructor(bd){
+        this._bd = bd;
+    }
+}
+
+module.exports = tikTokDAO;
---
+++ src/app/controllers/con_ttk.js
@@ -0,0 +1,6 @@
+const tikTokDAO = require("../bd/dao_ttk");
+const bd = require("../../config/database");
+
+class tiktokCON {}
+
+module.exports = tiktokCON;
