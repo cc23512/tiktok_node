da91203112bcc54755607f9e89f0202e2e8cf468
cc23512 <cc23512@g.unicamp.br>
2023-12-09 23:14:43 -0300

    form cadastro de ususario funcionando

--- views/public/scripts/index.js
+++ views/public/scripts/index.js
@@ -5,3 +5,19 @@ function abrirModal() {
 function fecharModal() {
   document.getElementById("modal").style.display = "none";
 }
+
+function abreCadastroFormulario() {
+  // ocultar form login
+  document.getElementById("loginForm").style.display = "none";
+
+  // mostra form login
+  document.getElementById("cadastroForm").style.display = "block";
+}
+
+function voltaParaLoginForm() {
+  // oculta form cadsastro
+  document.getElementById("cadastroForm").style.display = "none";
+
+  // mostra form cadastro
+  document.getElementById("loginForm").style.display = "block";
+}
