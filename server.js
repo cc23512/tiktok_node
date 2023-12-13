8d5a7ac172bec67ea47e0b176a5d7efacd124545
cc23512 <cc23512@g.unicamp.br>
2023-12-13 12:37:38 -0300

    Initial commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require("./src/config/express");
 const port = 3000;
 
 app.listen(port, function () {
-    console.log("Servidor na porta " + port);
-})
+  console.log("Servidor na porta " + port);
+});
